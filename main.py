0615071ff7b103d896cc8177b93d1a2668e55652
deusrex <arteyor123@gmail.com>
2026-03-03 17:19:40 +0000

    function calling 1

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
-
+from prompts import system_prompt
+from call_function import available_functions
 def main():
     parser = argparse.ArgumentParser(description="AI Code Assistant")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
@@ -29,6 +29,7 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt),
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
@@ -37,7 +38,11 @@ def generate_content(client, messages, verbose):
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
