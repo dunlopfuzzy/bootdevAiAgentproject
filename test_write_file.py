da6c9a354cefa314a793b596b625f149f2c5f151
deusrex <arteyor123@gmail.com>
2026-03-03 15:32:08 +0000

    write file

---
+++ test_write_file.py
@@ -0,0 +1,17 @@
+from functions.write_file import write_file
+
+def test():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result for writing to 'lorem.txt':")
+    print(result)
+
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result for writing to 'pkg/morelorem.txt':")
+    print(result)
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result for writing to '/tmp/temp.txt' directory:")
+    print(result)
+
+if __name__ == "__main__":
+    test()
